00fb7e701240061cfa67dbd4f7d9702b8ec30384
Younganswer <dudtj1999@gmail.com>
2023-02-14 19:52:30 +0900

    .

--- srcs/raytracing/specular.c
+++ srcs/raytracing/specular.c
@@ -2,47 +2,42 @@
 #include <math.h>
 
 t_vec3			specular(t_var *var, t_hit hit);
-static double	get_specular(t_light *light, t_hit hit);
+static double	get_specular_ratio(t_light *light, t_hit hit);
 
 t_vec3	specular(t_var *var, t_hit hit)
 {
 	t_list	*tmp;
 	t_light	*light;
-	t_vec3	ret;
+	double	spec_ratio;
 
-	ret = (t_vec3){0, 0, 0};
 	if (hit.object == NULL)
-		return (ret);
+		return ((t_vec3){0, 0, 0});
+	spec_ratio = 0;
 	tmp = var->lights;
 	while (tmp)
 	{
 		light = tmp->content;
-		ret = vec3_add(
-			ret,
-			vec3_mul(
-				get_origin_color(hit.object),
-				get_specular(light, hit) * light->ratio
-			)
-		);
+		spec_ratio += get_specular_ratio(light, hit) * light->ratio;
 		tmp = tmp->next;
 	}
-	return (ret);
+	return (vec3_mul(get_origin_color(hit.object), spec_ratio));
 }
 
-static double	get_specular(t_light *light, t_hit hit)
+static double	get_specular_ratio(t_light *light, t_hit hit)
 {
 	const t_vec3	contact = vec3_add(hit.ray.origin, hit.ray.direction);
-	const t_ray		light_dir = (t_ray){contact, vec3_sub(light->origin, contact)};
+	const t_ray		light_ray = (t_ray){contact, vec3_sub(light->origin, contact)};
 	const t_vec3	reflect = vec3_sub(
 		vec3_mul(
 			hit.normal.direction,
-			2 * vec3_dot(hit.normal.direction, light_dir.direction)
+			2 * vec3_dot(hit.normal.direction, light_ray.direction)
 		),
-		light_dir.direction
+		light_ray.direction
 	);
-	const double	dot = vec3_dot(reflect, vec3_reverse(vec3_unit(hit.ray.direction)));
+	const double	dot = vec3_dot(
+		vec3_unit(reflect),
+		vec3_reverse(vec3_unit(hit.ray.direction))
+	);
 
-	if (dot < 0)
-		return (0);
-	return (pow(fmax(0, dot), 100));
+	return (pow(fmax(0, dot), 50));
 }
