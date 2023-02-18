55fa809560774a304b4117f2964f452059422abd
Younganswer <dudtj1999@gmail.com>
2023-02-18 11:45:39 +0900

    .

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -2,29 +2,29 @@
 #include "../../incs/render.h"
 #include "../../incs/raytracing.h"
 
-// const size_t	g_samples = 25;
-// const size_t	g_row = 5;
-// const size_t	g_col = 5;
-// const double	g_weight[5][5] = {
-// 	{1.0, 4.0, 6.0, 4.0, 1.0},
-// 	{4.0, 16.0, 24.0, 16.0, 4.0},
-// 	{6.0, 24.0, 36.0, 24.0, 6.0},
-// 	{4.0, 16.0, 24.0, 16.0, 4.0},
-// 	{1.0, 4.0, 6.0, 4.0, 1.0}
-// };
-// const double	g_offset = 0.2;
-// const double	g_whole_weight = 256.0;
-
-const size_t	g_samples = 9;
-const size_t	g_row = 3;
-const size_t	g_col = 3;
-const double	g_weight[3][3] = {
-	{1.0, 2.0, 1.0},
-	{2.0, 4.0, 2.0},
-	{1.0, 2.0, 1.0}
+const size_t	g_samples = 25;
+const size_t	g_row = 5;
+const size_t	g_col = 5;
+const double	g_weight[5][5] = {
+	{1.0, 4.0, 6.0, 4.0, 1.0},
+	{4.0, 16.0, 24.0, 16.0, 4.0},
+	{6.0, 24.0, 36.0, 24.0, 6.0},
+	{4.0, 16.0, 24.0, 16.0, 4.0},
+	{1.0, 4.0, 6.0, 4.0, 1.0}
 };
-const double	g_offset = 0.3;
-const double	g_whole_weight = 16.0;
+const double	g_offset = 0.2;
+const double	g_whole_weight = 256.0;
+
+// const size_t	g_samples = 9;
+// const size_t	g_row = 3;
+// const size_t	g_col = 3;
+// const double	g_weight[3][3] = {
+// 	{1.0, 2.0, 1.0},
+// 	{2.0, 4.0, 2.0},
+// 	{1.0, 2.0, 1.0}
+// };
+// const double	g_offset = 0.3;
+// const double	g_whole_weight = 16.0;
 
 int				render(t_var *var);
 uint			anti_aliasing(t_var *var, double row, double col);
@@ -58,7 +58,6 @@ uint	anti_aliasing(t_var *var, double row, double col)
 	color = (t_vec3){0, 0, 0};
 	while ((size_t)++i < g_samples)
 		color = vec3_add(color, get_color(var, row, col, i));
-	color = vec3_div(color, g_whole_weight);
 	if (clamp(&color, 0, 255) == FALSE)
 		return (0);
 	return (vec3_to_color(color));
@@ -76,7 +75,7 @@ static t_vec3	get_color(t_var *var, double row, double col, int idx)
 	const t_hit		hit = hit_object(var, ray);
 	const t_vec3	ret = phong_reflection(var, hit);
 
-	return (vec3_mul(ret, g_weight[row_offset][col_offset]));
+	return (vec3_mul(ret, g_weight[row_offset][col_offset] / g_whole_weight));
 }
 
 static t_bool	clamp(t_vec3 *ret, uint min, uint max)
