967f2d32669c61e062ac54b0ad3525627cdcb88a
Younganswer <dudtj1999@gmail.com>
2023-02-22 15:21:37 +0900

    .

--- srcs/raytracing/cylinder.c
+++ srcs/raytracing/cylinder.c
@@ -3,10 +3,28 @@
 
 double			get_distance_to_cylinder(void *object, t_ray ray);
 t_ray			get_normal_of_cylinder(void *object, t_vec3 contact);
+static double	get_distance_to_base(t_ray normal, t_ray ray, double radius);
+static double	get_distance_to_curved(t_cylinder *cylinder, t_ray ray);
 
 double	get_distance_to_cylinder(void *object, t_ray ray)
 {
-	
+	t_cylinder *const	cylinder = (t_cylinder *) object;
+	const double	dist_to_bottom = get_distance_to_base(
+			(t_ray){cylinder->center, cylinder->normal},
+			ray,
+			cylinder->radius
+			);
+	const double	dist_to_top = get_distance_to_base(
+			(t_ray){
+				vec3_add(cylinder->center, vec3_mul(cylinder->normal, cylinder->height)),
+				vec3_reverse(cylinder->normal)
+			},
+			ray,
+			cylinder->radius
+			);
+	const double	dist_to_curved = get_distance_to_curved(cylinder, ray);
+
+	return (fmin(fmin(dist_to_bottom, dist_to_top), dist_to_curved));
 }
 
 t_ray	get_normal_of_cylinder(void *object, t_vec3 contact)
@@ -24,5 +42,47 @@ t_ray	get_normal_of_cylinder(void *object, t_vec3 contact)
 		ret.direction = cylinder->normal;
 	else
 		ret.direction = vec3_unit(normal);
+	return (ret);
+}
+
+static double	get_distance_to_base(t_ray normal, t_ray ray, double radius)
+{
+	const t_vec3	oc = vec3_sub(normal.origin, ray.origin);
+	const double	denom = vec3_dot(normal.direction, ray.direction);
+	const double	numer = vec3_dot(vec3_sub(oc, ray.origin), normal.direction);
+	double			ret;
+	double			dist_to_center;
+
+	if (fabs(denom) <= INFSIMAL)
+		return (INF);
+	ret = numer / denom;
+	if (ret < 0)
+		return (INF);
+	dist_to_center = vec3_length(
+			vec3_sub(
+				vec3_add(ray.origin, vec3_mul(ray.direction, ret)),
+				normal.origin
+				)
+			);
+	if (radius < dist_to_center)
+		return (INF);
+	return (ret);
+}
+
+static double	get_distance_to_curved(t_cylinder *cylinder, t_ray ray)
+{
+	const t_vec3	oc = vec3_sub(ray.origin, cylinder->center);
+	const double	a = vec3_dot(ray.direction, ray.direction) - pow(vec3_dot(ray.direction, cylinder->normal), 2);
+	const double	half_b = vec3_dot(oc, ray.direction) - vec3_dot(oc, cylinder->normal) * vec3_dot(ray.direction, cylinder->normal);
+	const double	c = vec3_dot(oc, oc) - pow(vec3_dot(oc, cylinder->normal), 2) - pow(cylinder->radius, 2);
+	double			ret;
+
+	if (pow(half_b, 2) < a * c)
+		return (INF);
+	ret = (-half_b - sqrt(pow(half_b, 2) - a * c)) / a;
+	if (ret < 0)
+		return (INF);
+	if (cylinder->height < vec3_dot(vec3_add(ray.origin, vec3_mul(ray.direction, ret)), cylinder->normal))
+		return (INF);
 	return (ret);
 }
--- srcs/raytracing/plane.c
+++ srcs/raytracing/plane.c
@@ -7,15 +7,12 @@ t_ray	get_normal_of_plane(void *object, t_vec3 contact);
 double	get_distance_to_plane(void *object, t_ray ray)
 {
 	t_plane *const	plane = (t_plane *) object;
+	const double	denom = vec3_dot(plane->normal, ray.direction);
+	const double	numer = vec3_dot(vec3_sub(plane->point, ray.origin), plane->normal);
 	double			ret;
-	double			denom;
-	double			numer;
 
-	ret = INF;
-	denom = vec3_dot(plane->normal, ray.direction);
 	if (fabs(denom) <= INFSIMAL)
-		return (ret);
-	numer = vec3_dot(vec3_sub(plane->point, ray.origin), plane->normal);
+		return (INF);
 	ret = numer / denom;
 	if (ret <= 0)
 		return (INF);
