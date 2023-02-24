fbb78e4085ff0eece975d97283b9f49808e6f33f
Sohyun Lee <sohlee@c3r4s6.42seoul.kr>
2023-02-24 15:14:44 +0900

    .

---
+++ libs/libft/objs/ft_atoi.d
@@ -0,0 +1,12 @@
+objs/ft_atoi.o: srcs/ft_atoi.c srcs/../incs/libft.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/stddef.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/__stddef_max_align_t.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/sys/cdefs.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/sys/_symbol_aliasing.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/sys/_posix_availability.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/machine/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/i386/limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/i386/_limits.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/sys/syslimits.h
---
+++ objs/miniRT.d
@@ -0,0 +1,10 @@
+objs/miniRT.o: srcs/miniRT.c srcs/../libs/libft/incs/libft.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/stddef.h \
+  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/12.0.0/include/__stddef_max_align_t.h \
+  srcs/../libs/libmlx/incs/mlx.h srcs/../incs/structs.h \
+  incs/../libs/libft/incs/libft.h incs/../libs/libvec/incs/vec3.h \
+  libs/libvec/incs/../../libft/incs/libft.h \
+  incs/../libs/libray/incs/libray.h \
+  incs/../libs/libray/incs/../../libvec/incs/vec3.h srcs/../incs/parse.h \
+  incs/structs.h srcs/../incs/render.h srcs/../incs/err.h \
+  srcs/../incs/event.h
